df0e634053373f1991ab384e2430d8890d46b1a6
Mike Jeffryes <mike.jeffryes@hotmail.com>
2021-02-10 13:48:08 +0000

    Added plotting of raw vs lin env vs approx emg channels

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Michael Jeffryes',
     author_email='mike.jeffryes@hotmail.com',
     url='',
-    version='1.0.3',
+    version='1.0.4',
     description='Analyses emg muscle synergies ',
     #packages=['emgmsa'],
     py_modules=["MSA",],
--- src/MSA.py
+++ src/MSA.py
@@ -1,9 +1,9 @@
 import json
 import numpy as np
 import scipy.signal as signal
-
 from sklearn.decomposition import NMF
 import matplotlib.pyplot as plt
+
 
 class ProccessChannel:
 
@@ -17,7 +17,7 @@ class ProccessChannel:
         self.aLP, self.bLP = signal.butter(4, fCritLP, 'low', analog=True)
         self.dsFrequency = dsFrequency
 
-    def go(self, emgChannel):
+    def run(self, emgChannel):
         channel = np.array(emgChannel)
         hpFiltered = self.highPassFilter(channel)
         rectified = self.rectify(hpFiltered)
@@ -25,27 +25,6 @@ class ProccessChannel:
         downsampled = self.downsample(lpFiltered)
         normalised = self.nomalise(downsampled)
         processedChannel = normalised
-        
-        
-        # plt.figure(figsize=(10,10))
-        # plt.subplot(6,1,1)
-        # plt.plot(channel)
-
-        # plt.subplot(6,1,2)
-        # plt.plot(hpFiltered)
-
-        # plt.subplot(6,1,3)
-        # plt.plot(rectified)
-
-        # plt.subplot(6,1,4)
-        # plt.plot(lpFiltered)
-
-        # plt.subplot(6,1,5)
-        # plt.plot(downsampled)
-
-        # plt.subplot(6,1,6)
-        # plt.plot(normalised)
-
         return processedChannel
 
     def highPassFilter(self, channel):
@@ -77,31 +56,62 @@ class ProcessInputData:
 
     def __init__(self, emgData, emgFrequency=1000, dsFrequency=100): 
         self.dsFrequency=dsFrequency
-
         self.emgFrequency=emgFrequency
-        self.processEMG(emgData)
+        self.emgData = emgData
 
-        # need to add function to select channels
-        self.nmfReady()
+        # Self preprocess EMG data
+        self.processInputData()
+
+        # Get labels and data array for the model
+        self.nmfDataReady()
         return
     
-    def processEMG(self, emgData):
+    def processInputData(self):
+        # create channel processor
         emgProcessor = ProccessChannel(dsFrequency=self.dsFrequency)
-        self.processedEMGData ={}
-        for key, value in emgData.items():
-            self.processedEMGData[key] = emgProcessor.go(value)
-        return
 
-    def nmfReady(self):
+        self.inputData = {}
+        for key, value in self.emgData.items():
+            self.inputData[key] = emgProcessor.run(value)
+        return
+    
+    def nmfDataReady(self):
         labels =[]
         data = []
-        for key, value in self.processedEMGData.items():
+        for key, value in self.inputData.items():
             labels.append(key)
             data.append(value)
         self.modelLabels = labels
         self.modelData = np.array(data)
         return
+    
+    def compareRawProcessedEMG(self):
+        
+        xraw = np.linspace(0, len(self.emgData[self.modelLabels[0]])+1, num=len(self.emgData[self.modelLabels[0]]))/self.emgFrequency
+        xlin = np.linspace(0, len(self.inputData[self.modelLabels[0]])+1, num=len(self.inputData[self.modelLabels[0]]))/self.dsFrequency 
+        xticks = np.linspace(0, max(xlin), num=6)
+        xlabs = np.linspace(0, 100, num=6)
 
+        for i, key in enumerate(self.modelLabels):
+
+            rawEmg = self.emgData[key]
+            linEnv = self.inputData[key]
+
+            fig, ax = plt.subplots()
+            ax.plot(xraw,rawEmg, color="blue")
+            ax.set_ylim([-0.55, 0.55])
+            ax.set_xlabel('Gait Cycle (%)')
+            ax.set_ylabel('Raw EMG Amplitude (V)', color="blue" , fontsize=11)
+            ax.set_title(f'Raw vs. processed {key} channel', fontsize=14)
+            ax.set_xticks(xticks)
+            ax.set_xticklabels(xlabs)
+
+            ax2=ax.twinx()
+            ax2.plot(xlin, linEnv, color="red")
+            ax2.set_ylabel("Processed EMG",color="red",fontsize=11)
+            ax2.set_ylim([-1.1,1.1])
+            plt.show()
+        return
 
 class RunModel:
     def __init__(self, processedData, noSynergies):
@@ -143,52 +153,57 @@ class RunModel:
     def calculateWALK_DMC(self, referenceTVAF1avg: float, referenceTVAF1stdev: float):
 
         self.walkDMC = 100+10*((referenceTVAF1avg-self.TVAF)/referenceTVAF1stdev)
+        return
 
-
-class MSAtrial:
+class MSAtrial(ProcessInputData):
     def __init__(self, emgData, dsFrequency=100, refTVAFavg=None, refTVAFstdev=None):
-
-        self.processedEMG = ProcessInputData(emgData, dsFrequency=dsFrequency).modelData
-
-        self.data = self.processedEMG
+        
         self.nCrit = 90
+        self.rawEmgData = emgData
+        # Process raw emg data
+        ProcessInputData.__init__(self, self.rawEmgData, dsFrequency=dsFrequency)
 
+        # Calculate tVAF1 (and walk-DMC if refs provided)
         self.TVAFS =[]
+        self.calculateSingleSynergy(refTVAFavg, refTVAFstdev)
 
-        self.singleSnergy(refTVAFavg, refTVAFstdev)
+        # Calculate tVAF for a range of msyn pairs
+        self.calculateMultipleSynergies()
 
-        self.multipleSynergies()
+        # Interpolate and calulate the N90
+        self.findN90()
+        return
+
+    def calculateSingleSynergy(self, refTVAFavg, refTVAFstdev):
+        msyn1 = RunModel(self.modelData, 1)
+        if (refTVAFavg!=None) and (refTVAFstdev!=None):  
+            self.walkDMC = msyn1.calculateWALK_DMC(refTVAFavg, refTVAFstdev)
+        else:
+            pass
+        self.TVAFS.append(msyn1.TVAF)
+        return
+
+    def calculateMultipleSynergies(self):
+
+        for i in range(2,4):
+            msyns = RunModel(self.modelData, i)
+            self.TVAFS.append(msyns.TVAF)
+        
+        i = i+1
+        while(np.mean(self.TVAFS[-3:])<99.5):
+            msyns = RunModel(self.modelData, i)
+            self.TVAFS.append(msyns.TVAF)
+            i =i+1
+            if i>49:
+                break
+        return
+
+
 
         self.findN90()
 
         return
         
-    def singleSnergy(self, refTVAFavg, refTVAFstdev):
-        msyn1 = RunModel(self.processedEMG, 1)
-        if (refTVAFavg!=None) and (refTVAFstdev!=None):  
-            self.walkDMC = msyn1.calculateWALK_DMC(refTVAFavg, refTVAFstdev)
-        else:
-            pass
-        
-        self.TVAFS.append(msyn1.TVAF)
-        return
-
-    def multipleSynergies(self):
-
-        for i in range(2,4):
-            msyns = RunModel(self.processedEMG, i)
-            self.TVAFS.append(msyns.TVAF)
-        
-        i = i+1
-        while(np.mean(self.TVAFS[-3:])<99.5):
-            msyns = RunModel(self.processedEMG, i)
-            self.TVAFS.append(msyns.TVAF)
-            i =i+1
-            if i>49:
-                break
-        
-        return
-
     def findN90(self):
         if self.TVAFS[0]<self.nCrit:
             for i, value in enumerate(self.TVAFS):
@@ -221,67 +236,153 @@ class MSAtrial:
         plt.show()
         return
 
-# ##################################
-# emgpath = "../tests/exampledata/t1_EMG_Left.json"
-# with open(emgpath, 'rb') as f:
-#     rawEMG = json.load(f)
+    def compareInputApprox(self):
 
-# goodCHN = ['RMG',"RTA", "RMH","RRF", 'LMG',"LTA", "LMH","LRF"]
-
-# EMG ={}
-# for c in goodCHN:
-#     EMG[c] = rawEMG[c]
-
-# # test chn process
-# keys = list(EMG.keys())
-
-# CHN = np.array(EMG[keys[0]])
-
-# q = ProccessChannel()
-# em = q.go(CHN)
-
-# # test process input
-# tr = ProcessInputData(EMG).modelData
-# tr.shape
+        noPairs = int(self.N90)
+        msyn = RunModel(self.modelData, noPairs)
 
 
-# ###########################################
-# ################################################
-# emgpath = "../tests/exampledata/t1_EMG_Left.json"
-# with open(emgpath, 'rb') as f:
-#     rawEMG = json.load(f)
+        xinput = np.linspace(0, len(self.modelData[0,:])+1, num=len(self.modelData[0,:]))/self.dsFrequency
+        xticks = np.linspace(0, max(xinput), num=6)
+        xlabs = np.linspace(0, 100, num=6)
 
-# tr = ProcessInputData(rawEMG).modelData
-# tr.shape
+        for i, key in enumerate(self.modelLabels):
+
+            inputEmg = self.modelData[i]
+            approxEmg = msyn.M[i]
+
+            fig, ax = plt.subplots()
+            ax.plot(xinput,inputEmg, color="blue")
+            ax.plot(xinput,approxEmg, color="red")
+            ax.set_ylim([-0.1, 1.1])
+            ax.set_xlabel('Gait Cycle (%)')
+            ax.set_ylabel('EMG Amplitude', fontsize=11)
+            ax.set_title(f'Input EMG vs. Approx. EMG {key} channel', fontsize=14)
+            ax.set_xticks(xticks)
+            ax.set_xticklabels(xlabs)
+            plt.show()
+        return
 
 
-# nmfModel = RunModel(tr, 1)
 
-# ####################################
 
-# ################################################
-# emgpath = "../tests/exampledata/t1_EMG_Left.json"
-# with open(emgpath, 'rb') as f:
-#     rawEMG = json.load(f)
 
-# processedEMG = ProcessInputData(rawEMG).modelData
-
-# trial = MSAtrial(rawEMG)
-# trial = MSAtrial(tr)
-# trial.N90
-# trial.plotN90()
-
-# nmfModel = RunModel(processedEMG, 1)
 
 # ####################################
 
 # from ezc3d import c3d
 # from gaittrial import TrialData
 
-# pth = "F:/msc_data/C3D_FILES_REF/SUB352_1_2.c3d"
+
+# # pth = "F:/msc_data/C3D_FILES_REF/SUB352_1_2.c3d"
+# # pth = "F:/msc_data/C3D_FILES_REF/SUB284_3_1.c3d"
+
+
+# pth= "F:\msc_data\C3D_FILES_REF\MSC_NORM_2.c3d"
 # trialc3d = c3d(pth)
 # tr = TrialData(trialc3d)
+# rawemg = tr.emgLeft
+# rawemg = {}
+# for key, value in tr.emgLeft.items():
 
-# msa = MSAtrial(tr.emgLeft)
+#     if ("LRF" in key) or ("LMH" in key) or ("LTA" in key) or ("LMG" in key) or ("LSOL" in key):
+#         pass
+#     else:
+#         rawemg[key] = value
+
+
+
+
+# pth= "F:\msc_data\C3D_FILES_REF\MSC_NORM_5.c3d"
+# trialc3d = c3d(pth)
+# tr = TrialData(trialc3d)
+# rawemg = tr.emgLeft
+# rawemg = {}
+# for key, value in tr.emgLeft.items():
+
+#     if ("RSOL" in key) or ("RMH" in key) or ("RRF" in key):
+#         pass
+#     else:
+#         rawemg[key] = value
+
+
+
+# p = ProcessInputData(rawemg)
+
+
+# p.compareRawProcessedEMG()
+
+# msa = MSAtrial(rawemg)
+
+# msa.compareRawProcessedEMG()
+# msa.compareInputApprox()
+
+# msa.N90
+
+# msa.plotN90()
+
+
+
+# ########
+
+
+
+
+# # pth = 'F:/msc_data/C3D_FILES_REF/SUB014_1_2.c3d'
+# # pth = 'F:/msc_data/C3D_FILES_REF/SUB014_1_3.c3d'
+# # pth = 'F:/msc_data/C3D_FILES_REF/SUB014_1_1.c3d'
+# # pth = 'F:/msc_data/C3D_FILES_REF/SUB014_1_4.c3d'
+
+
+
+# pth = "F:/msc_data/C3D_FILES_REF/SUB284_3_1.c3d"
+
+# trialc3d = c3d(pth)
+
+# analogchannels = np.transpose(np.array([trialc3d['parameters']['ANALOG']['LABELS']['value'], trialc3d['parameters']['ANALOG']['DESCRIPTIONS']['value']]))
+# analogdata = trialc3d['data']['analogs'][0]
+
+
+# for i, chn in enumerate(analogchannels):
+#     plt.figure()
+#     plt.title(f'raw data {chn[0]}')
+#     plt.plot(analogdata[i])
+#     plt.show()
+
+
+# full = tr.emgLeft
+# l ={}
+# r ={}
+# for key,value in full.items():
+#     if key[0] == 'L':
+#         l[key] = value
+#     else:
+#         r[key] = value
+
+
+
+
+# for key, value in full.items():
+
+#     plt.figure()
+#     plt.title(f'selected data {key}')
+#     plt.plot(value)
+#     plt.show()
+
+
+# msa = MSAtrial(full)
+
+# msaL = MSAtrial(l).plotN90()
+# msaR = MSAtrial(r).plotN90()
+
+# del full['LTA']
+# del full['LMG']
+
+
+# for key, value in tr.emgLeft.items():
+#     plt.figure()
+#     plt.title(key)
+#     plt.plot(value)
+#     plt.show()
 
 
